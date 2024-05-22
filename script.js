b61060daa1b7a61ef32b213d1e823afa752fdad0
github-userna-m <161277604+github-userna-m@users.noreply.github.com>
2024-05-22 21:39:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+
+  document.getElementById("mybtn").addEventListener("click", function() {
+    alert("Hello World!");
+    let svg = document.getElementsById("hellow");
+     if (svg.style.display === "none") {
+          svg.style.display = "block";
+      } else {
+          svg.style.display = none;
+      }
+
+
+  });
+
+
